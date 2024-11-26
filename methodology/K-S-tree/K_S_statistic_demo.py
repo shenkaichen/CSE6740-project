985c035d2be69045eb379b01726ca38d28fbb9e0
Kaichen Shen <shenkaichen0628@outlook.com>
2024-11-26 16:32:40 -0500

    complete logistic regression, random forest and RGBoost, update K-S statistic related

--- methodology/K-S-tree/K_S_statistic_demo.py
+++ methodology/K-S-tree/K_S_statistic_demo.py
@@ -1,31 +1,40 @@
 import numpy as np
 import pandas as pd
 
-# 示例数据
+
+# example dataframe with continuous feature
 data = {
     'feature': [0.1, 0.4, 0.35, 0.8, 0.5, 0.9, 0.7],
     'target': [0, 1, 1, 0, 1, 0, 1]
 }
 df = pd.DataFrame(data)
-
-print(df)
-print()
-
-# 步骤 1: 按特征值排序
 df = df.sort_values(by='feature')
-
 print(df)
-print()
-
-# 步骤 2: 计算正类和负类累积分布函数（CDF）
-df['cum_positive'] = (df['target'] == 1).cumsum() / (df['target'] == 1).sum()  # 正类累积占比
-df['cum_negative'] = (df['target'] == 0).cumsum() / (df['target'] == 0).sum()  # 负类累积占比
-
-# 步骤 3: 计算 K-S 统计量
+# use CDF to calculate K-S statistic if the feature is continuous
+df['cum_positive'] = (df['target'] == 1).cumsum() / (df['target'] == 1).sum()
+df['cum_negative'] = (df['target'] == 0).cumsum() / (df['target'] == 0).sum()
 df['diff'] = np.abs(df['cum_positive'] - df['cum_negative'])
-ks_statistic = df['diff'].max()
-
-# 打印结果
 print(df)
+ks_statistic = df['diff'].max()
+print("K-S Statistic (if feature is continuous): ", ks_statistic)
 print()
-print("K-S Statistic:", ks_statistic)
+
+
+# example dataframe with categorical/binary feature
+data = {
+    'feature': [2, 2, 2, 2, 1, 1, 1, 2],
+    'target': [0, 1, 1, 0, 1, 0, 1, 0]
+}
+df = pd.DataFrame(data)
+df = df.sort_values(by='feature')
+print(df)
+# use proportion to calculate K-S statistic if the feature is categorical/binary
+category_stats = df.groupby('feature')['target'].value_counts(normalize=False).unstack(fill_value=0)
+label_counts = df['target'].value_counts()
+category_stats[0] = category_stats[0] / label_counts[0]
+category_stats[1] = category_stats[1] / label_counts[1]
+category_stats['abs_diff'] = (category_stats[0] - category_stats[1]).abs()
+print(category_stats)
+ks_statistic = category_stats['abs_diff'].max()
+print("K-S Statistic (if feature is categorical/binary): ", ks_statistic)
+print()
